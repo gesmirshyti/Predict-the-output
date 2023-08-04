663d0b90bb3509283da8c6fdc62f118fb8597a37
gesmirshyti <gersi.shyti01@gmail.com>
2023-08-04 12:24:51 +0200

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-async function getCoderData() {
-    
-    var response = await fetch("https://api.github.com/users/gesmirshyti");
-    var coderData = await response.json();
-    console.log(coderData);
-} 
-getCoderData();
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
