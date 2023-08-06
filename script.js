ec596d013477e3611038a8359834f47c767df2b4
gesmirshyti <gersi.shyti01@gmail.com>
2023-08-06 16:10:15 +0200

    Ticking Clock

--- script.js
+++ script.js
@@ -1,51 +1,31 @@
-let currentNumber = "0";
-let operator = null;
-let prevNumber = null;
-
-function press(num) {
-    if (currentNumber === "0" || currentNumber === null) {
-        currentNumber = num.toString();
-    } else {
-        currentNumber += num.toString();
-    }
-    updateDisplay();
-}
-
-function setOP(op) {
-    operator = op;
-    prevNumber = currentNumber;
-    currentNumber = null;
-}
-
-function calculate() {
-    if (prevNumber !== null && operator !== null && currentNumber !== null) {
-        switch (operator) {
-            case '+':
-                currentNumber = (parseFloat(prevNumber) + parseFloat(currentNumber)).toString();
-                break;
-            case '-':
-                currentNumber = (parseFloat(prevNumber) - parseFloat(currentNumber)).toString();
-                break;
-            case '*':
-                currentNumber = (parseFloat(prevNumber) * parseFloat(currentNumber)).toString();
-                break;
-            case '/':
-                currentNumber = (parseFloat(prevNumber) / parseFloat(currentNumber)).toString();
-                break;
-        }
-        operator = null;
-        prevNumber = null;
-        updateDisplay();
-    }
-}
-
-function clr() {
-    currentNumber = "0";
-    operator = null;
-    prevNumber = null;
-    updateDisplay();
-}
-
-function updateDisplay() {
-    document.getElementById("display").textContent = currentNumber;
-}
+function getSecondsSinceStartOfDay() {
+    return new Date().getSeconds() + 
+      new Date().getMinutes() * 60 + 
+      new Date().getHours() * 3600;
+  }
+  
+  function getHourHandAngle(s) {
+    s %= 43200; // seconds in 12 hours
+    return ((360 * s / 43200) + 180) % 360;
+  }
+  
+  function getMinuteHandAngle(s) {
+    s %= 3600; // seconds in an hour
+    return ((6 * s / 60) + 180) % 360;
+  }
+  
+  function getSecondHandAngle(s) {
+    s %= 60; // seconds in a minute
+    return ((6 * s) + 180) % 360;
+  }
+  
+  const hour = document.getElementById("hour");
+  const minutes = document.getElementById("minutes");
+  const seconds = document.getElementById("seconds");
+  
+  setInterval( () => {
+    let s = getSecondsSinceStartOfDay();
+    hour.style.transform = `rotate(${getHourHandAngle(s)}deg)`;
+    minutes.style.transform = `rotate(${getMinuteHandAngle(s)}deg)`;
+    seconds.style.transform = `rotate(${getSecondHandAngle(s)}deg)`;
+  }, 1000);
